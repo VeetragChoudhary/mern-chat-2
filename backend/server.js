5903bb891a2203ec651fedc681347e07f0466e78
VeetragChoudhary <veetragchoudhary@gmail.com>
2024-07-28 15:22:03 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import path from 'path'
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import cors from "cors"
+app.use(cors())
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
@@ -15,6 +17,7 @@ const PORT = process.env.PORT || 5000
 const __dirname = path.resolve()
 
 dotenv.config()
+
 
 app.use(express.json())
 app.use(cookieParser())
